d0577006c87efae39f4fc7669fe4e2b87d2e6af2
Alexander Witkowski <alexanderwitkowski@gmail.com>
2016-10-14 14:58:21 +0200

    Hello click implementation with mvvm light libs

---
+++ UwpWorkshop/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace UwpWorkshop.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string text;
+        private int count;
+
+        public MainViewModel()
+        {
+            this.Text = "Hallo Open Space";
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { this.Set(ref text, value); }
+        }
+
+        public ICommand ClickCommand => new RelayCommand(this.HandleClickCommand);
+
+        private void HandleClickCommand()
+        {
+            this.count++;
+            this.Text = $"Hallo {this.count}";
+        }
+    }
+}
---
+++ UwpWorkshop/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace UwpWorkshop.ViewModels
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+}
